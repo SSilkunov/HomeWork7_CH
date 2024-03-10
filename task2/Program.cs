6204de0b717e2b6dbc1cb981e5a8f13eaf6c8ae3
Sergey <hmnu9415@yandex.ru>
2024-03-10 14:43:20 +0300

    make some changes in task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,19 +2,18 @@
 
 int AckermanFunction(int n, int m)
 {
-    if(n == 0)
-    return m+1;
+    if (n == 0)
+        return m + 1;
     else
-    if(m == 0)
-    return AckermanFunction(n-1,1);
+    if (m == 0)
+        return AckermanFunction(n - 1, 1);
     else
-    //if(n > 0 && m > 0)
-    return AckermanFunction(n-1, AckermanFunction(n, m-1));
+        return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
 }
 
 Console.WriteLine("Input first number:");
 int n = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Input second number:");
 int m = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Result: " + AckermanFunction(n,m));
+Console.WriteLine("Result: " + AckermanFunction(n, m));
 
