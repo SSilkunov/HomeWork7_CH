b06593b00355b0f9729d258403d4771cdaec671c
Sergey <hmnu9415@yandex.ru>
2024-03-10 18:21:13 +0300

    add more chenges in task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,19 +1,19 @@
 ﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-int AckermanFunction(int n, int m)
+int AckermanFunction(int m, int n)
 {
-    if (n == 0)
-        return m + 1;
-    else
     if (m == 0)
-        return AckermanFunction(n - 1, 1);
+        return n + 1;
     else
-        return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
+    if (n == 0)
+        return AckermanFunction(m - 1, 1);
+    else
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
 }
 
 Console.WriteLine("Input first number:");
-int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Input second number:");
 int m = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"A({n},{m})= " + AckermanFunction(n, m));
+Console.WriteLine("Input second number:");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"A({m},{n})= " + AckermanFunction(n, m));
 
